3f2a06b1bbddad9d9abf4bce04557008de7256b6
Mohaiminul-Islam-Bhuiyan <rafe.aust@gmail.com>
2022-10-12 22:19:01 +0600

    chart added

--- src/components/Statics.jsx
+++ src/components/Statics.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import { TopicContext } from './Root';
+
 
 const Statics = () => {
+    const data = useContext(TopicContext)
     return (
-        <div>
-            <h2>this is stat</h2>
-        </div>
+        <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+        <Line type="monotone" dataKey="total" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+      </LineChart>
     );
 };
 
